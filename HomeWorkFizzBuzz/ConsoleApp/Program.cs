69e6c0dae2b39f6647b292b987ccf58335693f4f
Kurilo Dmitriy <fobosdem@gmail.com>
2019-09-29 12:33:01 +0300

    Creating project

---
+++ HomeWorkFizzBuzz/ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathClass;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Random rnd = new Random();
+			int[] array = new int[rnd.Next(10000, 25000)];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = i;
+			}
+
+			MathFizBuzFibo mathFizBuzFibo = new MathFizBuzFibo();
+			Console.WriteLine("Enter number of finding fibonachi number");
+			int number = int.Parse(Console.ReadLine());
+			Task.Run(() =>
+			{
+				Stopwatch watch = new Stopwatch();
+
+				watch.Start();
+				Console.WriteLine(MathFizBuzFibo.Fibonachi(number));
+				watch.Stop();
+
+				Console.WriteLine($"fibonachi time {watch.Elapsed.ToString()}");
+
+			});
+
+			Task.Run(() =>
+			{
+				var oneArraySym = mathFizBuzFibo.FizzBuzzOneArray(array);
+				//Console.WriteLine(oneArraySym);
+			});
+
+			Task.Run(() =>
+			{
+				var twoArraySym = mathFizBuzFibo.FizzBuzzAsyncTwoArrays(array);
+				//Console.WriteLine(twoArraySym);
+			});
+
+			Console.ReadKey();
+
+		}
+	}
+}
---
+++ HomeWorkFizzBuzz/MathClass/MathFizBuzFibo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+	public class MathFizBuzFibo
+	{
+		public string FizzBuzzAsyncTwoArrays(int[] array)
+		{
+			Stopwatch watch = new Stopwatch();
+
+			watch.Start();
+
+			string result = "";
+			int halfOfArray = array.Length / 2;
+			int[] firstPart = new int[halfOfArray];
+			int[] secondPart = new int[halfOfArray];
+
+			for (int i = 0, j = halfOfArray; i < halfOfArray; i++, j++)
+			{
+				firstPart[i] = array[i];
+				secondPart[i] = array[j];
+			}
+
+			var firstResult = Result(firstPart);
+			var secondResult = Result(secondPart);
+
+
+			result += firstResult.Result;
+			result += secondResult.Result;
+			watch.Stop();
+
+			Console.WriteLine($"2 arrays time{watch.Elapsed.ToString()}");
+			return result;
+		}
+
+		public string FizzBuzzOneArray(int[] array)
+		{
+			Stopwatch watch = new Stopwatch();
+
+			watch.Start();
+			string result = Result(array).Result;
+			watch.Stop();
+
+			Console.WriteLine($"1 arrays time{watch.Elapsed.ToString()}");
+			return result;
+
+		}
+
+		private async Task<string> Result(int[] array, int firstValue = 3, int secondValue = 5)
+		{
+
+			string result = "";
+			foreach (int element in array)
+			{
+				if (((element % firstValue) == 0) && ((element % secondValue) == 0))
+				{
+					result += "FizzBuzz";
+				}
+				else if ((element % firstValue) == 0)
+				{
+					result += "Fizz";
+				}
+				else if ((element % secondValue) == 0)
+				{
+					result += "Buzz";
+				}
+				else
+				{
+					result += $"{element}";
+				}
+			}
+			await AsyncSimulation();
+			return result;
+		}
+		private async Task AsyncSimulation()
+		{
+			await Task.Run(() => Thread.Sleep(1));
+		}
+
+		public static int Fibonachi(int n)
+		{
+			if (n == 0 || n == 1)
+			{
+				return n;
+			}
+			else
+			{
+				return Fibonachi(n - 1) + Fibonachi(n - 2);
+			}
+		}
+	}
+}
